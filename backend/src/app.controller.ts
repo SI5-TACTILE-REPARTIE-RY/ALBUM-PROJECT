30108144295541ff067a29de617dacd70b81f6e1
Romain Giuntini <romain.giuntini@etu.univ-cotedazur.fr>
2021-02-10 00:37:23 +0100

    Ajout de logs

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -9,26 +9,31 @@ export class AppController {
 
   @Get()
   getHello(): string {
+    console.log('REST :: EMPTY GET :: Hello world');
     return this.appService.getHello();
   }
 
   @Get('session')
   getSession(): Session {
+    console.log(`REST :: GET :: SESSION :: ${CurrentSession}`);
     return CurrentSession;
   }
 
   @Get('start-album-session')
   startAlbumSession(): void {
+    console.log('REST :: GET :: START ALBUM SESSION');
     this.appService.startAlbumSession();
   }
 
   @Get('stop-album-session')
   stopAlbumSession(): void {
+    console.log('REST :: GET :: STOP ALBUM SESSION');
     this.appService.stopAlbumSession();
   }
 
   @Get('apply-filter/:filterName')
   applyFilter(@Param('filterName') filterName: string): void {
+    console.log(`REST :: GET :: APPLY FILTER :: ${filterName}`);
     this.appService.applyFilter(filterName);
   }
 }
--- backend/src/ws/ws.gateway.ts
+++ backend/src/ws/ws.gateway.ts
@@ -12,29 +12,35 @@ export class WsGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer() server;
 
   async handleConnection() {
+    console.log('GATEWAY :: CONNECTION');
     CurrentSession.users++;
     this.server.emit('users', CurrentSession.users);
   }
 
   async handleDisconnect() {
+    console.log('GATEWAY :: DISCONNECT');
     CurrentSession.users--;
     this.server.emit('users', CurrentSession.users);
   }
 
   @SubscribeMessage('message')
   async onWsMessage(client, message) {
+    console.log(`GATEWAY :: MESSAGE EVENT :: ${message}`);
     client.broadcast.emit('message', message);
   }
 
   async albumSessionStarted(photoSrc: string) {
+    console.log(`GATEWAY :: START ALBUM SESSION`);
     this.server.emit('album-session-started', photoSrc);
   }
 
   async albumSessionStopped() {
+    console.log(`GATEWAY :: STOP ALBUM SESSION`);
     this.server.emit('album-session-stopped');
   }
 
   async filterApplied(filterName: string) {
+    console.log(`GATEWAY :: APPLY FILTER :: ${filterName}`);
     this.server.emit('filter-applied', filterName);
   }
 }
